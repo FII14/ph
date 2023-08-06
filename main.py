f76c2cf9de6d19d8327acce588db6fcce0ec5fe9
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 13:11:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
-# Program : Pembuat Hash [MD5, SHA-1, SHA-256, SHA-512]
-# Pembuat : Rofi [FII14]
-# GitHub  : https://github.com/FII14/ph
-
 import hashlib
 
 print("""
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@ Program : Pembuat Hash [MD5, SHA-1, SHA-256, SHA-512] @
-@ Pembuat : Rofi [FII14]                                @
-@ GitHub  : https://github.com/FII14/ph                 @
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Program : Pembuat hash [MD5, SHA1, SHA256, SHA512] @
+@ Pembuat : Rofi [FII14]                             @
+@ GitHub  : https://github.com/FII14/ph              @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 """)
+
 input_teks = input("Masukkan teks yang ingin di-hash: ")
+
 if not input_teks:
     print("Input teks kosong. Tidak ada hash yang dihasilkan.")
+
 else:
     algoritma_hash = ["md5", "sha1", "sha256", "sha512"]   
     print("")
+    
     for algoritma in algoritma_hash:        
         if algoritma == "md5":
             objek_hash = hashlib.md5()
@@ -28,9 +28,12 @@ else:
             objek_hash = hashlib.sha256()
         elif algoritma == "sha512":
             objek_hash = hashlib.sha512()        
+        
         else:
             raise ValueError("Algoritma hash tidak valid")
+        
         objek_hash.update(input_teks.encode())
         hasil_hash = objek_hash.hexdigest()
         print(f"[*] Hasil hash {algoritma.upper()}: {hasil_hash}")
+    
     print("")
